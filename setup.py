29394792133542ea61e3b42d4b50774881d33ad8
lorenzomarini96 <lorenzo.marini.1996@gmail.com>
2022-07-04 12:23:58 +0200

    Filled setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='microcal_classifier',
+      version='1.0',
+      description='Convolutional neural networks for the computing methods for experimental physics and data analysis (CMEPDA) course.',
+      author='Lorenzo Marini',
+      author_email='lorenzo.marini.1996@gmail.com',
+      url='https://github.com/lorenzomarini96/nome_mai_usato',
+      packages=['distutils', 'distutils.command'],
+     )
