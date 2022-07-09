fd26a124097436dc1c8b01edf6cb2697c8e90530
lorenzomarini96 <lorenzo.marini.1996@gmail.com>
2022-07-09 17:50:27 +0200

    dovs

--- microcal_classifier/module_template.py
+++ microcal_classifier/module_template.py
@@ -4,16 +4,19 @@ def func1(arg1, arg2):
     """
     The func1 function implements the scalar sum between
     two addends given in input.
+
     Parameters
     ----------
-    arg1 : int, float
-           Description
-    arg1 : int, float
-           Description
+    arg1 : int
+        The first parameter.
+    arg1 : int
+        The second parameter.
+
     Returns
     -------
-    sum : int, float
-          Return the sum of arg1 with arg2.
+    int
+        Return the sum of arg1 with arg2.
+
     """
 
     return arg1 + arg2
