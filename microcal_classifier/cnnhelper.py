26aacaf46d2b3f9b45a447f74c010d94f34fcc14
lorenzomarini96 <lorenzo.marini.1996@gmail.com>
2022-07-11 11:21:11 +0200

    test on docs

--- microcal_classifier/cnnhelper.py
+++ microcal_classifier/cnnhelper.py
@@ -24,25 +24,38 @@ logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
 
-file_handler = logging.FileHandler("CNN_model.log")
+file_handler = logging.FileHandler("CNN_helper.log")
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 
 
 def bar_plot(y_train, y_test):
-    """Function to make a horizontal bar plot
+    """Plot an horizontal bar graph
     showing the relative partition of the two given dataset.
 
-    Args:
-        y_train (array): Numpy array with the label (0,1) for the train data set.
-        y_test (array): Numpy array with the label (0,1) for the test data set.
+    Parameters
+    ----------
+    y_train : numpy_array
+        Labels for the train data set.
+    y_test : numpy_array 
+        Labels for the train data set.
 
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> y_train = np.array([1, 0, 0, 1, 1])
+    >>> y_test = np.array([1, 1, 0, 1, 1])
+    >>> bar_plot(y_train, y_test)
     """
 
-    data = {'0': [sum(map(lambda x : x == 0, y_train)),sum(map(lambda x : x == 0, y_test))],
-          '1': [sum(map(lambda x : x == 1, y_train)),sum(map(lambda x : x == 1, y_test))]
-          }
+    data = {'0': [sum(map(lambda x : x == 0, y_train)), sum(map(lambda x : x == 0, y_test))],
+            '1': [sum(map(lambda x : x == 1, y_train)), sum(map(lambda x : x == 1, y_test))]
+           }
     df = pd.DataFrame(data,columns=['0','1'], index = ['Train','Test'])
     df.plot.barh()
     plt.title('Train and Test dataset partitions')
@@ -52,14 +65,31 @@ def bar_plot(y_train, y_test):
 
 
 def barplot_split_set(y_train, y_test, val_perc=0.30):
-    """Function plotting an hypothetical partition of the dataset in train, validation and test.
+    """Plot a graph bar showing partition of the dataset for train, validation and test.
 
-    Args:
-        y_train (array): Numpy array with the label (0,1) for the train data set.
-        y_test (array): Numpy array with the label (0,1) for the test data set.
-        val_perc (float, optional): Percentual of items to use in validation.
+    Parameters
+    ----------
+    y_train : numpy_array
+        Labels for the train data set.
+    y_test : numpy_array 
+        Labels for the train data set.
+    val_perc : float, optional
+        Percentual of items to use in validation.
         Defaults to 0.30.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> y_train = np.array([1, 0, 0, 1, 1])
+    >>> y_test = np.array([1, 1, 0, 1, 1])
+    >>> PERCENTUAL = 0.30
+    >>> bar_plot(y_train, y_test,  val_perc=PERCENTUAl)
     """
+
     data = {'0': [sum(map(lambda x : x == 0, y_train)) * (1 - val_perc),
             sum(map(lambda x : x == 0, y_train)) * val_perc,
             sum(map(lambda x : x == 1, y_test))],
@@ -79,19 +109,34 @@ def barplot_split_set(y_train, y_test, val_perc=0.30):
 def count_labels(y_train, y_test, verbose=True):
     """Count the number of items in the set.
 
-    Args:
-        y_train (array): Array of labels for train.
-        y_train (array): Array of labels for train.
-        verbose (bool, optional): Print the number of items in the set. Defaults to True.
+    Parameters
+    ----------
+    y_train : numpy_array
+        Labels for the train data set.
+    y_test : numpy_array 
+        Labels for the test data set.
+    verbose : bool, optional
+        Print the number of items in the set. Defaults to True.
 
-    Returns:
-        int: DataFrame containing the total number of items for a specific label and set.
+    Returns
+    -------
+    data : pandas dataframe.
+        Dataframe containing the total number of items for a specific label and set.
+    
+    Examples
+    --------
+    >>> import numpy as np
+    >>> y_train = np.array([1, 0, 0, 1, 1])
+    >>> y_test = np.array([1, 1, 0, 1, 1])
+            0    1
+    Train   2    3
+    Test    1    4
     """
     count_0_train = sum(map(lambda label : label == 0, y_train))
     count_1_train = sum(map(lambda label : label == 1, y_train))
     count_0_test = sum(map(lambda label : label == 0, y_test))
     count_1_test = sum(map(lambda label : label == 1, y_test))
- 
+
     dict = {'Label': ['0', '1'],
         'Train': [count_0_train, count_1_train],
         'Test': [count_0_test, count_1_test]}
@@ -104,14 +149,35 @@ def count_labels(y_train, y_test, verbose=True):
     return data
 
 
-def plot_random_image(X, y, n_x=3, n_y=3):
+def plot_random_images(X, y, n_x=3, n_y=3):
     """Plot an image with given label randomly selected from a given dataset.
 
-    Args:
-        X (array): Numpy array containg the image values.
-        y (array): Numpy arrays containg the label for each image.
-        n_x (int): Number of images in the rows.
-        n_y (int): Number of images in the columns.
+    Parameters
+    ----------
+    X : numpy_array
+        Image pixel intensity values.
+    y : numpy_array
+        Labels for the train data set.
+    n_x : int
+        Number of images in the rows.
+    n_y : int
+        Number of images in the columns.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+
+    >>> import matplotlib.image as mpimg
+    >>> import matplotlib.pyplot as plt
+    >>> import numpy as np
+    >>> imgs = ['img1.pgm', 'img2.pgm', 'img3.pgm', 'img4.pgm']
+    >>> X = np.array([mpimg.imread(i), for i in imgs])
+    >>> labels = [1, 0, 0, 1]
+    >>> N_X, N_Y = (2, 2)
+    >>> plot_random_image(X=images, y=labels, n_x=N_X, n_y=N_Y)
     """
 
     plt.figure(figsize=(10,10))
@@ -141,8 +207,8 @@ def read_imgs(dataset_path, classes):
         array: Numpy array containing the value of image/label.
 
     Examples:
-        >>> train_dataset_path = '/content/gdrive/MyDrive/DATASETS_new/IMAGES/Mammography_micro/Train'
-        >>> x_train, y_train = read_imgs(train_dataset_path, [0, 1])
+        >>> TRAIN_DATA_PATH = '/path/to/train/folder'
+        >>> x_train, y_train = read_imgs(TRAIN_DATA_PATH, [0, 1])
 
     """
     tmp = []
@@ -164,10 +230,10 @@ def split_dataset(X_train, y_train, X_test, y_test, perc=0.2, verbose=True):
     """Split the train dataset for train and validation set.
 
     Args:
-        X_train (array): Images of train set.
-        y_train (array): Labels of train set.
-        X_test (array): Images of test set.
-        y_test (array): Labels of test set.
+        X_train (numpy_array): Images of train set.
+        y_train (numpy_array): Labels of train set.
+        X_test (numpy_array): Images of test set.
+        y_test (numpy_array): Labels of test set.
         perc (float): Percentual of items for the validation set.
 
     Returns:
@@ -200,7 +266,10 @@ def split_dataset(X_train, y_train, X_test, y_test, perc=0.2, verbose=True):
 
     return X_train, X_val, y_train, y_val
 
-'''
+
+
+
+
 if __name__ == '__main__':
 
     TRAIN_DATASET_PATH = '/home/lorenzomarini/Desktop/DATASETS_new/IMAGES/Mammography_micro/Train'
@@ -214,4 +283,3 @@ if __name__ == '__main__':
     #count_labels(y_train, y_test, verbose=True)
     #plot_random_image(X_train, y_train, nx=3, ny=3)
     split_dataset(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, perc=0.25, verbose=True)
-'''
