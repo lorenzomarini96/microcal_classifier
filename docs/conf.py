3c503b83ad52830a1b66619f2431555ef8f7711c
lorenzomarini96 <lorenzo.marini.1996@gmail.com>
2022-07-16 12:22:39 +0200

    work on doc of wavelet module

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,8 @@ autodoc_mock_imports = ["numpy",
                         "keras.preprocessing.image.array_to_img",
                         "keras.preprocessing.image.array_to_img.img_to_array",
                         "keras.preprocessing.image.array_to_img.load_img",
-                        "tensorflow"
+                        "tensorflow",
+                        "pywt"
                         ]
 
 # -- Project information -----------------------------------------------------
