ad071b507728e478b12f2d254774e3f7768e5662
lorenzomarini96 <lorenzo.marini.1996@gmail.com>
2022-07-17 12:54:27 +0200

    minor work using pylint on wavelet_coeff

--- microcal_classifier/wavelet_coeff.py
+++ microcal_classifier/wavelet_coeff.py
@@ -59,10 +59,10 @@ def dwtcoefftoarray(image, wavelet, level, partial=False):
     # ravel_coeffs: Wavelet transform coefficient array.
     # All coefficients have been concatenated into a single 1D array.
 
-    if partial == False:
+    if partial is False:
         wavecoeffs = infocoeffs[0]
 
-    if partial == True:
+    if partial is True:
         secondlevelcoeffs = np.concatenate((infocoeffs[0][infocoeffs[1][-2]['da']],
                                             infocoeffs[0][infocoeffs[1][-2]['ad']],
                                             infocoeffs[0][infocoeffs[1][-2]['dd']]))
@@ -90,11 +90,14 @@ def dwtcoefftoarray(image, wavelet, level, partial=False):
 
 def dwt_analysis(image, wavelet, level):
     # pylint: disable=C0301
+    # pylint: disable=W0632
     '''
     Decompose the original image by means of the multilevel 2D discrete wavelet transform
     with the selected wavelet family up to the fifth level.
     The obtained coefficients matrices are then masked keeping only those values which
     are greater than the standard deviation calculated over the matrix values.
+    Computing the standard deviation for each matrix (image and coefficients),
+    the std will act as a treshold: if abs(value) < 0. => value = 0, otherwise: value = value.
     Finally, the image is reconstructed using the new coefficients.
 
     Parameters
@@ -139,19 +142,13 @@ def dwt_analysis(image, wavelet, level):
     else:
         pass
 
-    '''
-    Compute the standard deviation for each matrix (image and coefficients).
-    The std will act as a treshold: if abs(value) < 0. => value = 0, otherwise: value = value
-    
-    https://pywavelets.readthedocs.io/en/latest/ref/thresholding-functions.html
-    Thresholds the input data depending on the mode argument.
-    
-    mode = 'hard': In hard thresholding, the data values where their absolute value
-    is less than the value param are replaced with substitute.
-    Data values with absolute value greater or equal to the thresholding value stay untouched.
-    '''
-
     ncA = np.zeros_like(cA) # No approximate image anymore
+
+    # Thresholds the input data depending on the mode argument.
+    # mode = 'hard': In hard thresholding, the data values where their
+    # absolute value is less than the value param are replaced with substitute.
+    # Data values with absolute value greater or equal to the thresholding value stay untouched.
+    # https://pywavelets.readthedocs.io/en/latest/ref/thresholding-functions.html
 
     std10 = np.std(cH1)
     std11 = np.std(cV1)
@@ -169,7 +166,8 @@ def dwt_analysis(image, wavelet, level):
     ncV2 = pywt.threshold(cV2, std21, mode='hard', substitute=0.)
     ncD2 = pywt.threshold(cD2, std22, mode='hard', substitute=0.)
 
-    if level == 3 or level == 4:
+    #if level == 3 or level == 4:
+    if level in (3, 4):
         std30 = np.std(cH3)
         std31 = np.std(cV3)
         std32 = np.std(cD3)
@@ -177,10 +175,12 @@ def dwt_analysis(image, wavelet, level):
         ncH3 = pywt.threshold(cH3, std30, mode='hard', substitute=0.)
         ncV3 = pywt.threshold(cV3, std31, mode='hard', substitute=0.)
         ncD3 = pywt.threshold(cD3, std32, mode='hard', substitute=0.)
+
     else:
         pass
 
-    if level == 4 or level == 5:
+    #if level == 4 or level == 5:
+    if level in (4, 5):
         std40 = np.std(cH4)
         std41 = np.std(cV4)
         std42 = np.std(cD4)
@@ -188,6 +188,7 @@ def dwt_analysis(image, wavelet, level):
         ncH4 = pywt.threshold(cH4, std40, mode='hard', substitute=0.)
         ncV4 = pywt.threshold(cV4, std41, mode='hard', substitute=0.)
         ncD4 = pywt.threshold(cD4, std42, mode='hard', substitute=0.)
+
     else:
         pass
 
@@ -199,6 +200,7 @@ def dwt_analysis(image, wavelet, level):
         ncH5 = pywt.threshold(cH5, std50, mode='hard', substitute=0.)
         ncV5 = pywt.threshold(cV5, std51, mode='hard', substitute=0.)
         ncD5 = pywt.threshold(cD5, std52, mode='hard', substitute=0.)
+
     else:
         pass
 
@@ -212,18 +214,16 @@ def dwt_analysis(image, wavelet, level):
     elif level == 5:
         new_coeff = ncA, (ncH5, ncV5, ncD5), (ncH4, ncV4, ncD4), (ncH3, ncV3, ncD3), (ncH2, ncV2, ncD2), (ncH1, ncV1, ncD1)
 
-    '''
-    2D multilevel image reconstruction using the new coefficients and waverec2:
-    The waverec2 function reconstructs the image from a set of given coefficient
-    returning the 2D array of reconstructed data.
-    '''
+    # 2D multilevel image reconstruction using the new coefficients and waverec2:
+    # The waverec2 function reconstructs the image from a set of given coefficient
+    # returning the 2D array of reconstructed data.
 
     # Multilevel 2D Inverse Discrete Wavelet Transform.
     newimage = pywt.waverec2(new_coeff, wavelet)
     newimage = pywt.threshold(newimage, 0., mode = 'greater', substitute = 0.)
-    '''
-    In greater thresholding, the data is replaced with substitute ( = 0.)
-    where data is below the thresholding value.
-    Greater data values pass untouched.
-    '''
+
+    # In greater thresholding, the data is replaced with substitute ( = 0.)
+    # where data is below the thresholding value.
+    # Greater data values pass untouched.
+
     return newimage
