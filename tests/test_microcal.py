666eecc059a1a2e6ad17f9994b52ac95f4a91988
lorenzomarini96 <lorenzo.marini.1996@gmail.com>
2022-07-19 15:46:47 +0200

    Added new unit tests

--- tests/test_microcal.py
+++ tests/test_microcal.py
@@ -1,62 +1,66 @@
 """CMEPDA Project: Unittest"""
+## pylint: disable=C0103
+## pylint: disable=C0413
+
 import sys
 import os
 from pathlib import Path
 import warnings
 import unittest
 
-from sklearn.model_selection import train_test_split
+import numpy as np
 
-# Get the absolute path to the parent dir.
-sys.path.insert(0, str(Path(os.getcwd()).parent))
-
-package_name = "../microcal_classifier/"
-
-# setting path
-#sys.path.append('../microcal_classifier')
-
-sys.path.insert(0, package_name)
+# Setting path
+sys.path.insert(0, str(Path(os.getcwd()).parent)) # Get the absolute path to the parent dir.
+PACKAGE_NAME = "../microcal_classifier/"
+sys.path.insert(0, PACKAGE_NAME)
 
 from microcal_classifier.wavelethelper import read_img
 from microcal_classifier.cnnhelper import read_imgs, count_labels, split_dataset
+from microcal_classifier.wavelet_coeff import dwt_coeff_array
 
 
 class TestMicroClassifier(unittest.TestCase):
-    """Unittest for the microcal_classiofier project."""
+    """Unit test for the microcal_classifier project."""
 
     def setUp(self):
         """
         Initialize the class.
         """
         warnings.simplefilter('ignore', ResourceWarning)
-        
+
         self.PATH = '/home/lorenzomarini/Desktop/DATASETS_new/IMAGES/Mammography_micro/'
         self.TRAIN_PATH = '/home/lorenzomarini/Desktop/DATASETS_new/IMAGES/Mammography_micro/Train'
         self.TEST_PATH = '/home/lorenzomarini/Desktop/DATASETS_new/IMAGES/Mammography_micro/Test'
 
 
     def test_read_img_mp(self):
-        """Unittest for read img function with multiprocessing."""
-        
-        # error message in case if test case got failed
+        """Unit test for read img function with multiprocessing."""
+
         error_message_len = "Wrong lenght"
-        TRAIN_PATH_0 = os.path.join(self.PATH, 'Train/0')
-        X0_train, y0_train = read_img(TRAIN_PATH_0)
-        TRAIN_PATH_1 = os.path.join(self.PATH, 'Train/1')
-        X1_train, y1_train = read_img(TRAIN_PATH_1)
-        TEST_PATH_0 = os.path.join(self.PATH, 'Test/0')
-        X0_test, y0_test = read_img(TEST_PATH_0)
-        TEST_PATH_1 = os.path.join(self.PATH, 'Test/1')
-        X1_test, y1_test = read_img(TEST_PATH_1)
+        #error_message_shape = "Wrong shape"
+
+        train_path_0 = os.path.join(self.PATH, 'Train/0')
+        X0_train, y0_train = read_img(train_path_0)
+        train_path_1 = os.path.join(self.PATH, 'Train/1')
+        X1_train, y1_train = read_img(train_path_1)
+        test_path_0 = os.path.join(self.PATH, 'Test/0')
+        X0_test, y0_test = read_img(test_path_0)
+        test_path_1 = os.path.join(self.PATH, 'Test/1')
+        X1_test, y1_test = read_img(test_path_1)
 
         self.assertEqual(len(y0_train), 330, error_message_len)
         self.assertEqual(len(y1_train), 306, error_message_len)
         self.assertEqual(len(y0_test), 84, error_message_len)
         self.assertEqual(len(y1_test), 77, error_message_len)
-    
+        #self.assertEqual(X0_train.shape, (330, 60, 60, 1), error_message_len)
+        #self.assertEqual(X1_train.shape, (306, 60, 60, 1), error_message_len)
+        #self.assertEqual(X0_test.shape, (84, 60, 60, 1), error_message_shape)
+        #self.assertEqual(X1_test.shape, (77, 60, 60, 1) , error_message_shape)
+
 
     def test_read_imgs(self):
-        """Unittest for read img function (no multiprocessing)."""
+        """Unit test for read img function (no multiprocessing)."""
         error_message_shape = "Wrong shape"
 
         X_train, y_train = read_imgs(self.TRAIN_PATH, [0, 1])
@@ -66,7 +70,7 @@ class TestMicroClassifier(unittest.TestCase):
         self.assertEqual(X_test.shape, (161, 60, 60, 1), error_message_shape)
         self.assertEqual(y_train.shape, (636,), error_message_shape)
         self.assertEqual(y_test.shape, (161,) , error_message_shape)
-        
+
 
     def test_count_labels(self):
         """Unittest for count the number of labels in the data set."""
@@ -81,26 +85,56 @@ class TestMicroClassifier(unittest.TestCase):
 
 
     def test_split_dataset(self):
-        """Unittest for split the train dataset for train and validation set."""
+        """Unit test for split the train dataset for train and validation set."""
 
-        X_TRAIN_raw, y_TRAIN_raw = read_imgs(self.TRAIN_PATH, [0, 1])
-        X_TEST_raw, y_TEST_raw = read_imgs(self.TEST_PATH, [0, 1])
-        PERC = 0.2
+        x_train_raw, y_train_raw = read_imgs(self.TRAIN_PATH, [0, 1])
+        x_test_raw, y_test_raw = read_imgs(self.TEST_PATH, [0, 1])
+        perc = 0.2
         message = "Split not valid."
         delta = 0.09
-    
-        _, _, y_TRAIN, y_VAL = split_dataset(X_train=X_TRAIN_raw,
-                                             y_train=y_TRAIN_raw,
-                                             X_test=X_TEST_raw,
-                                             y_test=y_TEST_raw,
-                                             perc=PERC,
+
+        _, _, y_train, y_val = split_dataset(X_train=x_train_raw,
+                                             y_train=y_train_raw,
+                                             X_test=x_test_raw,
+                                             y_test=y_test_raw,
+                                             perc=perc,
                                              verbose=False
                                              )
 
-        self.assertAlmostEqual(PERC, len(y_VAL)/len(y_TRAIN), None, message, delta)
+        self.assertAlmostEqual(perc, len(y_val)/len(y_train), None, message, delta)
 
 
+    def test_dwt_coeff_array(self):
+        """Unit test for calculation of weavelet coefficients of the 2DWT."""
 
+        train_path_0 = os.path.join(self.PATH, 'Train/0')
+        X0_train, _ = read_img(train_path_0)
+        train_path_1 = os.path.join(self.PATH, 'Train/1')
+        X1_train, _ = read_img(train_path_1)
+        test_path_0 = os.path.join(self.PATH, 'Test/0')
+        X0_test, _ = read_img(test_path_0)
+        test_path_1 = os.path.join(self.PATH, 'Test/1')
+        X1_test, _ = read_img(test_path_1)
+
+        X_train = X0_train + X1_train
+        X_test = X0_test + X1_test
+        X_tot = X_test + X_train
+
+        wavelet = 'db5'
+        level = 4
+        partial = True
+        coefficients = []
+
+        for i, _ in enumerate(X_tot):
+            array = dwt_coeff_array(X_tot[i],
+                                    wavelet=wavelet,
+                                    level=level,
+                                    partial=partial
+                                    )
+            coefficients.append(array)
+        coefficients = np.array(coefficients)
+
+        self.assertEqual(coefficients.shape, (797, 2430), "Wrong shape in wavelet coefficient.")
 
 
 if __name__ == "__main__":
