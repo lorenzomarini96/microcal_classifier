4dd379a082e5640ff789baec668081d1f6d06784
lorenzomarini96 <lorenzo.marini.1996@gmail.com>
2022-07-20 01:37:00 +0200

    wrong url

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='microcal_classifier',
       description='Convolutional neural networks for the computing methods for experimental physics and data analysis (CMEPDA) course.',
       author='Lorenzo Marini',
       author_email='lorenzo.marini.1996@gmail.com',
-      url='https://github.com/lorenzomarini96/nome_mai_usato',
+      url='https://github.com/lorenzomarini96/microcal_classifier',
       packages=['distutils', 'distutils.command'],
      )
