6f0413468e44015c79b4c79c67b6e8ad07317acc
lorenzomarini96 <lorenzo.marini.1996@gmail.com>
2022-07-20 21:27:19 +0200

    solved unittest issue?

--- tests/test_microcal_ci.py
+++ tests/test_microcal_ci.py
@@ -9,6 +9,7 @@ import warnings
 import unittest
 
 import numpy as np
+import pywt
 
 # Locally: setting path
 #sys.path.insert(0, str(Path(os.getcwd()).parent)) # Get the absolute path to the parent dir.
@@ -33,78 +34,39 @@ class TestMicroClassifier(unittest.TestCase):
         """
         warnings.simplefilter('ignore', ResourceWarning)
 
+        self.x_train = np.random.rand(6, 6)
+        self.x_test = np.random.rand(6, 6)
+        self.y_train = np.array([0,0,1,1,0,0])
         self.y_test = np.array([1,1,1,0,0,0])
-        self.y_train = np.array([0,0,1,0,0,0])
+        self.x_train_wavelet = np.random.rand(60, 60)
+        self.x_test_wavelet = np.random.rand(60, 60)
+
 
     def test_count_labels(self):
         """Unittest for count the number of labels in the data set."""
-
 
         data = count_labels(self.y_train, self.y_test, verbose=False)
 
         self.assertIn('Train', data.keys(), 'Train not in dataframe')
         self.assertIn('Test', data.keys(), 'Test not in dataframe')
-        self.assertEqual(data.shape, (2, 3), 'Shape not correct')
-
-    '''
-    def test_read_img_mp(self):
-        """Unit test for read img function with multiprocessing."""
-
-        error_message_len = "Wrong lenght"
-
-        train_path_0 = os.path.join(self.PATH, 'Train/0')
-        _, y0_train = read_img(train_path_0)
-        train_path_1 = os.path.join(self.PATH, 'Train/1')
-        _, y1_train = read_img(train_path_1)
-        test_path_0 = os.path.join(self.PATH, 'Test/0')
-        _, y0_test = read_img(test_path_0)
-        test_path_1 = os.path.join(self.PATH, 'Test/1')
-        _, y1_test = read_img(test_path_1)
-
-        self.assertEqual(len(y0_train), 330, error_message_len)
-        self.assertEqual(len(y1_train), 306, error_message_len)
-        self.assertEqual(len(y0_test), 84, error_message_len)
-        self.assertEqual(len(y1_test), 77, error_message_len)
-
-
-    def test_read_imgs(self):
-        """Unit test for read img function (no multiprocessing)."""
-        error_message_shape = "Wrong shape"
-
-        X_train, y_train = read_imgs(self.TRAIN_PATH, [0, 1])
-        X_test, y_test = read_imgs(self.TEST_PATH, [0, 1])
-
-        self.assertEqual(X_train.shape, (636, 60, 60, 1), error_message_shape)
-        self.assertEqual(X_test.shape, (161, 60, 60, 1), error_message_shape)
-        self.assertEqual(y_train.shape, (636,), error_message_shape)
-        self.assertEqual(y_test.shape, (161,) , error_message_shape)
-
-
-    def test_count_labels(self):
-        """Unittest for count the number of labels in the data set."""
-
-        _, y_train = read_imgs(self.TRAIN_PATH, [0, 1])
-        _, y_test = read_imgs(self.TEST_PATH, [0, 1])
-        data = count_labels(y_train, y_test, verbose=False)
-
-        self.assertIn('Train', data.keys(), 'Train not in dataframe')
-        self.assertIn('Test', data.keys(), 'Test not in dataframe')
+        self.assertEqual(data['Train'][0], 4 , 'Number of 0 in train not correct')
+        self.assertEqual(data['Test'][0], 3 , 'Number of 0 in test not correct')
+        self.assertEqual(data['Train'][1], 2 , 'Number of 1 in train not correct')
+        self.assertEqual(data['Test'][1], 3 , 'Number of 1 in test not correct')
         self.assertEqual(data.shape, (2, 3), 'Shape not correct')
 
 
     def test_split_dataset(self):
         """Unit test for split the train dataset for train and validation set."""
 
-        x_train_raw, y_train_raw = read_imgs(self.TRAIN_PATH, [0, 1])
-        x_test_raw, y_test_raw = read_imgs(self.TEST_PATH, [0, 1])
         perc = 0.2
         message = "Split not valid."
-        delta = 0.09
+        delta = 0.3
 
-        _, _, y_train, y_val = split_dataset(X_train=x_train_raw,
-                                             y_train=y_train_raw,
-                                             X_test=x_test_raw,
-                                             y_test=y_test_raw,
+        _, _, y_train, y_val = split_dataset(X_train=self.x_train,
+                                             y_train=self.y_train,
+                                             X_test=self.x_test,
+                                             y_test=self.y_test,
                                              perc=perc,
                                              verbose=False
                                              )
@@ -115,26 +77,15 @@ class TestMicroClassifier(unittest.TestCase):
     def test_dwt_coeff_array(self):
         """Unit test for calculation of weavelet coefficients of the 2DWT."""
 
-        train_path_0 = os.path.join(self.PATH, 'Train/0')
-        X0_train, _ = read_img(train_path_0)
-        train_path_1 = os.path.join(self.PATH, 'Train/1')
-        X1_train, _ = read_img(train_path_1)
-        test_path_0 = os.path.join(self.PATH, 'Test/0')
-        X0_test, _ = read_img(test_path_0)
-        test_path_1 = os.path.join(self.PATH, 'Test/1')
-        X1_test, _ = read_img(test_path_1)
-
-        X_train = X0_train + X1_train
-        X_test = X0_test + X1_test
-        X_tot = X_test + X_train
+        x_tot = [self.x_train_wavelet, self.x_test_wavelet]
 
         wavelet = 'db5'
         level = 4
         partial = True
         coefficients = []
 
-        for i, _ in enumerate(X_tot):
-            array = dwt_coeff_array(X_tot[i],
+        for i, _ in enumerate(x_tot):
+            array = dwt_coeff_array(x_tot[i],
                                     wavelet=wavelet,
                                     level=level,
                                     partial=partial
@@ -142,8 +93,7 @@ class TestMicroClassifier(unittest.TestCase):
             coefficients.append(array)
         coefficients = np.array(coefficients)
 
-        self.assertEqual(coefficients.shape, (797, 2430), "Wrong shape in wavelet coefficient.")
-        '''
+        self.assertEqual(coefficients.shape, (2, 2430), "Wrong shape in wavelet coefficient.")
 
 
 if __name__ == "__main__":
